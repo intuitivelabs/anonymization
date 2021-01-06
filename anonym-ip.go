4971d451dd33a97e452c6024d1b74d1f0bacad9a
Cristian Constantin <cristian@intuitivelabs.com>
2021-01-06 22:02:14 +0100

    adds decrypt funcs, cipher.block interface, AUTs

--- anonym-ip.go
+++ anonym-ip.go
@@ -4,7 +4,8 @@
 // that can be found in the LICENSE.txt file in the root of the source
 // tree.
 
-// see ipcipher specification here:
+// IP address anonymization using block encryption.
+// For details, see ipcipher specification here:
 // https://powerdns.org/ipcipher/ipcipher.md.html
 
 package anonymization
@@ -19,7 +20,45 @@ import (
 
 var (
 	ErrBrokenIP = errors.New("broken IP address")
+	ErrKeySize  = errors.New("illegal key size")
 )
+
+const BlockSize = 16
+
+// Ipcipher implements cipher.Block interface
+type Ipcipher struct {
+	key [BlockSize]byte
+}
+
+func (c *Ipcipher) BlockSize() int { return BlockSize }
+
+func NewCipher(key []byte) (cipher.Block, error) {
+	switch k := len(key); k {
+	default:
+		return nil, ErrKeySize
+	case BlockSize:
+		break
+	}
+	var a [BlockSize]byte
+	for i := range key {
+		a[i] = key[i]
+	}
+	return &Ipcipher{a}, nil
+}
+
+func (c *Ipcipher) Encrypt(dst, src []byte) {
+	if err := EncryptIP(c.key, dst, src); err != nil {
+		panic("anonymization: encrypt error")
+	}
+	return
+}
+
+func (c *Ipcipher) Decrypt(dst, src []byte) {
+	if err := DecryptIP(c.key, dst, src); err != nil {
+		panic("anonymization: decrypt error")
+	}
+	return
+}
 
 // setIPv4Bytes sets the bytes of an IPv4 address preserving the format
 // (i.e. either plain IPv4 or IPv4 address represented as an IPv6 address)
@@ -36,8 +75,26 @@ func setIPv4Bytes(IP net.IP, b [4]byte) (err error) {
 	return
 }
 
-// encryptIPv4 anonymizes the "IP" address by encrypting it
-func encryptIPv4(key [16]byte, IP net.IP) (encryptedIP net.IP, err error) {
+// encryptIPv4 writes the anonymized (encrypted) IPv4 IP address into "encryptedIP"
+func encryptIPv4(key [BlockSize]byte, encryptedIP, IP net.IP) (err error) {
+	var c [net.IPv4len]byte
+	if c, err = ipcrypt.EncryptBin(key, IP); err == nil {
+		copy(encryptedIP, c[:])
+	}
+	return
+}
+
+// decryptIPv4 writes the plain (decrypted) IPv4 IP address into "IP"
+func decryptIPv4(key [BlockSize]byte, IP, encryptedIP net.IP) (err error) {
+	var c [net.IPv4len]byte
+	if c, err = ipcrypt.DecryptBin(key, encryptedIP); err == nil {
+		copy(IP, c[:])
+	}
+	return
+}
+
+// encryptedIPv4 returns the anonymized (encrypted) IPv4 address
+func encryptedIPv4(key [BlockSize]byte, IP net.IP) (encryptedIP net.IP, err error) {
 	var c [4]byte
 	if c, err = ipcrypt.EncryptBin(key, IP); err == nil {
 		encryptedIP = c[:]
@@ -45,7 +102,17 @@ func encryptIPv4(key [16]byte, IP net.IP) (encryptedIP net.IP, err error) {
 	return
 }
 
-func encryptIPv4InPlace(key [16]byte, IP net.IP) (err error) {
+// decryptedIPv4 returns the plain (decrypted) IPv4 address
+func decryptedIPv4(key [BlockSize]byte, encryptedIP net.IP) (IP net.IP, err error) {
+	var c [4]byte
+	if c, err = ipcrypt.DecryptBin(key, encryptedIP); err == nil {
+		IP = c[:]
+	}
+	return
+}
+
+// encryptIPv4InPlace anonymizes the IPv4 IP address by encrypting it in-place
+func encryptIPv4InPlace(key [BlockSize]byte, IP net.IP) (err error) {
 	var c [net.IPv4len]byte
 	if c, err = ipcrypt.EncryptBin(key, IP); err == nil {
 		err = setIPv4Bytes(IP, c)
@@ -53,7 +120,35 @@ func encryptIPv4InPlace(key [16]byte, IP net.IP) (err error) {
 	return
 }
 
-func encryptIPv6(key [16]byte, IP net.IP) (encryptedIP net.IP, err error) {
+// decryptIPv4InPlace uncovers the IPv4 IP address by decrypting it in-place
+func decryptIPv4InPlace(key [BlockSize]byte, encryptedIP net.IP) (err error) {
+	var c [net.IPv4len]byte
+	if c, err = ipcrypt.DecryptBin(key, encryptedIP); err == nil {
+		err = setIPv4Bytes(encryptedIP, c)
+	}
+	return
+}
+
+// encryptIPv6 writes the anonymized (encrypted) IPv6 IP address into "encryptedIP"
+func encryptIPv6(key [BlockSize]byte, encryptedIP, IP net.IP) (err error) {
+	var block cipher.Block
+	if block, err = aes.NewCipher(key[:]); err == nil {
+		block.Encrypt(encryptedIP, IP)
+	}
+	return
+}
+
+// decryptIPv6 writes the plain (decrypted) IPv6 IP address into "IP"
+func decryptIPv6(key [BlockSize]byte, IP, encryptedIP net.IP) (err error) {
+	var block cipher.Block
+	if block, err = aes.NewCipher(key[:]); err == nil {
+		block.Decrypt(IP, encryptedIP)
+	}
+	return
+}
+
+// encryptedIPv6 returns the anonymized (encrypted) IPv6 address
+func encryptedIPv6(key [BlockSize]byte, IP net.IP) (encryptedIP net.IP, err error) {
 	var block cipher.Block
 	if block, err = aes.NewCipher(key[:]); err != nil {
 		encryptedIP = []byte{}
@@ -64,7 +159,20 @@ func encryptIPv6(key [16]byte, IP net.IP) (encryptedIP net.IP, err error) {
 	return
 }
 
-func encryptIPv6InPlace(key [16]byte, IP net.IP) (err error) {
+// decryptedIPv6 returns the plain (decrypted) IPv6 address
+func decryptedIPv6(key [BlockSize]byte, encryptedIP net.IP) (IP net.IP, err error) {
+	var block cipher.Block
+	if block, err = aes.NewCipher(key[:]); err != nil {
+		IP = []byte{}
+	} else {
+		IP = make([]byte, net.IPv6len)
+		block.Decrypt(IP, encryptedIP)
+	}
+	return
+}
+
+// encryptIPv6InPlace anonymizes the IPv6 IP address by encrypting it in-place
+func encryptIPv6InPlace(key [BlockSize]byte, IP net.IP) (err error) {
 	var block cipher.Block
 	if block, err = aes.NewCipher(key[:]); err == nil {
 		var c [net.IPv6len]byte
@@ -74,24 +182,89 @@ func encryptIPv6InPlace(key [16]byte, IP net.IP) (err error) {
 	return
 }
 
-// EncryptIP returns the anonymized (encrypted) "IP" address
-func EncryptIP(key [16]byte, IP net.IP) (encryptedIP net.IP, err error) {
-	if IP.To4() != nil {
-		encryptedIP, err = encryptIPv4(key, IP)
-	} else if IP.To16() != nil {
-		encryptedIP, err = encryptIPv6(key, IP)
+// decryptIPv6InPlace uncovers the IPv6 IP address by decrypting it in-place
+func decryptIPv6InPlace(key [BlockSize]byte, encryptedIP net.IP) (err error) {
+	var block cipher.Block
+	if block, err = aes.NewCipher(key[:]); err == nil {
+		var c [net.IPv6len]byte
+		block.Decrypt(c[:], encryptedIP)
+		copy(encryptedIP, c[:])
+	}
+	return
+}
+
+// EncryptIP writes the anonymized (encrypted) IP address into encryptedIP
+func EncryptIP(key [BlockSize]byte, encryptedIP, IP net.IP) (err error) {
+	if IP.To4() != nil &&
+		(len(encryptedIP) == net.IPv4len ||
+			len(encryptedIP) == net.IPv6len) {
+		err = encryptIPv4(key, encryptedIP, IP)
+	} else if IP.To16() != nil &&
+		len(encryptedIP) == net.IPv6len {
+		err = encryptIPv6(key, encryptedIP, IP)
 	} else {
 		err = ErrBrokenIP
 	}
 	return
 }
 
-// EncryptIPInPlace anonymizes the "IP" address by encrypting it
-func EncryptIPInPlace(key [16]byte, IP net.IP) (err error) {
+// DecryptIP writes the plain (decrypted) IP address into IP
+func DecryptIP(key [BlockSize]byte, IP, encryptedIP net.IP) (err error) {
+	if encryptedIP.To4() != nil &&
+		(len(IP) == net.IPv4len ||
+			len(IP) == net.IPv6len) {
+		err = decryptIPv4(key, IP, encryptedIP)
+	} else if encryptedIP.To16() != nil &&
+		len(IP) == net.IPv6len {
+		err = decryptIPv6(key, IP, encryptedIP)
+	} else {
+		err = ErrBrokenIP
+	}
+	return
+}
+
+// EncryptedIP returns the anonymized (encrypted) IP address
+func EncryptedIP(key [BlockSize]byte, IP net.IP) (encryptedIP net.IP, err error) {
+	if IP.To4() != nil {
+		encryptedIP, err = encryptedIPv4(key, IP)
+	} else if IP.To16() != nil {
+		encryptedIP, err = encryptedIPv6(key, IP)
+	} else {
+		err = ErrBrokenIP
+	}
+	return
+}
+
+// DecryptedIP returns the plain (decrypted) IP address
+func DecryptedIP(key [BlockSize]byte, encryptedIP net.IP) (IP net.IP, err error) {
+	if encryptedIP.To4() != nil {
+		IP, err = decryptedIPv4(key, encryptedIP)
+	} else if encryptedIP.To16() != nil {
+		IP, err = decryptedIPv6(key, encryptedIP)
+	} else {
+		err = ErrBrokenIP
+	}
+	return
+}
+
+// EncryptIPInPlace anonymizes the "IP" address by encrypting it in-place
+func EncryptIPInPlace(key [BlockSize]byte, IP net.IP) (err error) {
 	if IP.To4() != nil {
 		err = encryptIPv4InPlace(key, IP)
 	} else if IP.To16() != nil {
 		err = encryptIPv6InPlace(key, IP)
+	} else {
+		err = ErrBrokenIP
+	}
+	return
+}
+
+// DecryptIPInPlace uncovers the "encryptedIP" address by de-encrypting it in-place
+func DecryptIPInPlace(key [BlockSize]byte, encryptedIP net.IP) (err error) {
+	if encryptedIP.To4() != nil {
+		err = decryptIPv4InPlace(key, encryptedIP)
+	} else if encryptedIP.To16() != nil {
+		err = decryptIPv6InPlace(key, encryptedIP)
 	} else {
 		err = ErrBrokenIP
 	}
---
+++ anonym-ip_test.go
@@ -0,0 +1,341 @@
+package anonymization
+
+import (
+	"bytes"
+	"net"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// initialize whatever needs to be initialized
+	os.Exit(m.Run())
+}
+
+func TestEncryptIP(t *testing.T) {
+	// set-up if needed
+	t.Run("IPv4, passphrase", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4},
+			[]byte{198, 41, 56, 22},
+			[]byte{22, 11, 33, 44},
+			[]byte{255, 0, 255, 241},
+		}
+		passphrase := "justapassword"
+		var key [16]byte
+		GenerateKeyFromPassphraseAndCopy(passphrase, key[:])
+		var enc, dec net.IP
+		enc = make([]byte, net.IPv4len)
+		dec = make([]byte, net.IPv4len)
+		for _, c := range cases {
+			if err := EncryptIP(key, enc, c); err != nil {
+				t.Fatalf("encryption error %s for IP %s", err, c.String())
+			}
+			if err := DecryptIP(key, dec, enc); err != nil {
+				t.Fatalf("decryption error %s for IP %s", err, c.String())
+			}
+			if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+	t.Run("IPv4, key", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4},
+			[]byte{198, 41, 56, 22},
+			[]byte{22, 11, 33, 44},
+			[]byte{255, 0, 255, 241},
+		}
+		var key [16]byte = [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+		var enc, dec net.IP
+		enc = make([]byte, net.IPv4len)
+		dec = make([]byte, net.IPv4len)
+		for _, c := range cases {
+			if err := EncryptIP(key, enc, c); err != nil {
+				t.Fatalf("encryption error %s for IP %s", err, c.String())
+			}
+			if err := DecryptIP(key, dec, enc); err != nil {
+				t.Fatalf("decryption error %s for IP %s", err, c.String())
+			}
+			if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+	t.Run("IPv6, passphrase", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4, 255, 0, 255, 241, 251, 6, 245, 231, 51, 60, 145, 231},
+			[]byte{198, 41, 56, 22, 123, 10, 133, 144, 98, 4, 11, 12, 13, 18, 15, 164},
+			[]byte{22, 11, 33, 44, 255, 30, 255, 241, 20, 101, 6, 8, 250, 0, 75, 61},
+			[]byte{255, 0, 255, 241, 210, 20, 155, 241, 75, 40, 235, 221, 225, 50, 215, 141},
+		}
+		passphrase := "anotherlongpassword"
+		var key [16]byte
+		GenerateKeyFromPassphraseAndCopy(passphrase, key[:])
+		var enc, dec net.IP
+		enc = make([]byte, net.IPv6len)
+		dec = make([]byte, net.IPv6len)
+		for _, c := range cases {
+			if err := EncryptIP(key, enc, c); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			}
+			if err := DecryptIP(key, dec, enc); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			}
+			if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+	t.Run("IPv6, key", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4, 255, 0, 255, 241, 251, 6, 245, 231, 51, 60, 145, 231},
+			[]byte{198, 41, 56, 22, 123, 10, 133, 144, 98, 4, 11, 12, 13, 18, 15, 164},
+			[]byte{22, 11, 33, 44, 255, 30, 255, 241, 20, 101, 6, 8, 250, 0, 75, 61},
+			[]byte{255, 0, 255, 241, 210, 20, 155, 241, 75, 40, 235, 221, 225, 50, 215, 141},
+		}
+		var key [16]byte = [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+		var enc, dec net.IP
+		enc = make([]byte, net.IPv6len)
+		dec = make([]byte, net.IPv6len)
+		for _, c := range cases {
+			if err := EncryptIP(key, enc, c); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			}
+			if err := DecryptIP(key, dec, enc); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			}
+			if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+}
+
+func TestDecryptIP(t *testing.T) {
+	// encryption and decryption are symmetrical
+	TestEncryptIP(t)
+}
+
+func TestEncryptedIP(t *testing.T) {
+	// set-up if needed
+	t.Run("IPv4, passphrase", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4},
+			[]byte{198, 41, 56, 22},
+			[]byte{22, 11, 33, 44},
+			[]byte{255, 0, 255, 241},
+		}
+		passphrase := "justapassword"
+		var key [16]byte
+		GenerateKeyFromPassphraseAndCopy(passphrase, key[:])
+		for _, c := range cases {
+			if enc, err := EncryptedIP(key, c); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			} else if dec, err := DecryptedIP(key, enc); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			} else if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+	t.Run("IPv4, key", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4},
+			[]byte{198, 41, 56, 22},
+			[]byte{22, 11, 33, 44},
+			[]byte{255, 0, 255, 241},
+		}
+		var key [16]byte = [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+		for _, c := range cases {
+			if enc, err := EncryptedIP(key, c); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			} else if dec, err := DecryptedIP(key, enc); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			} else if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+	t.Run("IPv6, passphrase", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4, 255, 0, 255, 241, 251, 6, 245, 231, 51, 60, 145, 231},
+			[]byte{198, 41, 56, 22, 123, 10, 133, 144, 98, 4, 11, 12, 13, 18, 15, 164},
+			[]byte{22, 11, 33, 44, 255, 30, 255, 241, 20, 101, 6, 8, 250, 0, 75, 61},
+			[]byte{255, 0, 255, 241, 210, 20, 155, 241, 75, 40, 235, 221, 225, 50, 215, 141},
+		}
+		passphrase := "anotherlongpassword"
+		var key [16]byte
+		GenerateKeyFromPassphraseAndCopy(passphrase, key[:])
+		for _, c := range cases {
+			if enc, err := EncryptedIP(key, c); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			} else if dec, err := DecryptedIP(key, enc); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			} else if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+	t.Run("IPv6, key", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4, 255, 0, 255, 241, 251, 6, 245, 231, 51, 60, 145, 231},
+			[]byte{198, 41, 56, 22, 123, 10, 133, 144, 98, 4, 11, 12, 13, 18, 15, 164},
+			[]byte{22, 11, 33, 44, 255, 30, 255, 241, 20, 101, 6, 8, 250, 0, 75, 61},
+			[]byte{255, 0, 255, 241, 210, 20, 155, 241, 75, 40, 235, 221, 225, 50, 215, 141},
+		}
+		var key [16]byte = [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+		for _, c := range cases {
+			if enc, err := EncryptedIP(key, c); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			} else if dec, err := DecryptedIP(key, enc); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			} else if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+}
+
+func TestDecryptedIP(t *testing.T) {
+	// encryption and decryption are symmetrical
+	TestEncryptedIP(t)
+}
+
+func TestEncryptIPInPlace(t *testing.T) {
+	// set-up if needed
+	t.Run("IPv4, passphrase", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4},
+			[]byte{198, 41, 56, 22},
+			[]byte{22, 11, 33, 44},
+			[]byte{255, 0, 255, 241},
+		}
+		passphrase := "justapassword"
+		var key [16]byte
+		GenerateKeyFromPassphraseAndCopy(passphrase, key[:])
+		var tmpIP net.IP
+		tmpIP = make([]byte, net.IPv4len)
+		for _, c := range cases {
+			copy(tmpIP, c)
+			if err := EncryptIPInPlace(key, tmpIP); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			} else if err := DecryptIPInPlace(key, tmpIP); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			} else if !bytes.Equal(tmpIP, c) {
+				t.Errorf("expected %s have %s:", c.String(), tmpIP.String())
+			}
+		}
+	})
+	t.Run("IPv4, key", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4},
+			[]byte{198, 41, 56, 22},
+			[]byte{22, 11, 33, 44},
+			[]byte{255, 0, 255, 241},
+		}
+		var key [16]byte = [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+		for _, c := range cases {
+			if enc, err := EncryptedIP(key, c); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			} else if dec, err := DecryptedIP(key, enc); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			} else if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+	t.Run("IPv6, passphrase", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4, 255, 0, 255, 241, 251, 6, 245, 231, 51, 60, 145, 231},
+			[]byte{198, 41, 56, 22, 123, 10, 133, 144, 98, 4, 11, 12, 13, 18, 15, 164},
+			[]byte{22, 11, 33, 44, 255, 30, 255, 241, 20, 101, 6, 8, 250, 0, 75, 61},
+			[]byte{255, 0, 255, 241, 210, 20, 155, 241, 75, 40, 235, 221, 225, 50, 215, 141},
+		}
+		passphrase := "anotherlongpassword"
+		var key [16]byte
+		GenerateKeyFromPassphraseAndCopy(passphrase, key[:])
+		for _, c := range cases {
+			if enc, err := EncryptedIP(key, c); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			} else if dec, err := DecryptedIP(key, enc); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			} else if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+	t.Run("IPv6, key", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4, 255, 0, 255, 241, 251, 6, 245, 231, 51, 60, 145, 231},
+			[]byte{198, 41, 56, 22, 123, 10, 133, 144, 98, 4, 11, 12, 13, 18, 15, 164},
+			[]byte{22, 11, 33, 44, 255, 30, 255, 241, 20, 101, 6, 8, 250, 0, 75, 61},
+			[]byte{255, 0, 255, 241, 210, 20, 155, 241, 75, 40, 235, 221, 225, 50, 215, 141},
+		}
+		var key [16]byte = [16]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+		for _, c := range cases {
+			if enc, err := EncryptedIP(key, c); err != nil {
+				t.Fatalf("encryption error: %s for IP %s", err, c.String())
+			} else if dec, err := DecryptedIP(key, enc); err != nil {
+				t.Fatalf("decryption error: %s for IP %s", err, c.String())
+			} else if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+}
+
+func TestDecryptIPInPlace(t *testing.T) {
+	// encryption and decryption are symmetrical
+	TestEncryptIPInPlace(t)
+}
+
+func TestEncrypt(t *testing.T) {
+	passphrase := "justalonglongpasswordforanonymization"
+	var key [16]byte
+	GenerateKeyFromPassphraseAndCopy(passphrase, key[:])
+	ipCipher, err := NewCipher(key[:])
+	if err != nil {
+		t.Errorf("ipcipher error: %s ", err)
+	}
+	t.Run("IPv4", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4},
+			[]byte{198, 41, 56, 22},
+			[]byte{22, 11, 33, 44},
+			[]byte{255, 0, 255, 241},
+		}
+		var enc, dec net.IP
+		enc = make([]byte, net.IPv4len)
+		dec = make([]byte, net.IPv4len)
+		for _, c := range cases {
+			ipCipher.Encrypt(enc, c)
+			ipCipher.Decrypt(dec, enc)
+			if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+	t.Run("IPv6", func(t *testing.T) {
+		cases := []net.IP{
+			[]byte{1, 2, 3, 4, 255, 0, 255, 241, 251, 6, 245, 231, 51, 60, 145, 231},
+			[]byte{198, 41, 56, 22, 123, 10, 133, 144, 98, 4, 11, 12, 13, 18, 15, 164},
+			[]byte{22, 11, 33, 44, 255, 30, 255, 241, 20, 101, 6, 8, 250, 0, 75, 61},
+			[]byte{255, 0, 255, 241, 210, 20, 155, 241, 75, 40, 235, 221, 225, 50, 215, 141},
+		}
+		var enc, dec net.IP
+		enc = make([]byte, net.IPv6len)
+		dec = make([]byte, net.IPv6len)
+		for _, c := range cases {
+			ipCipher.Encrypt(enc, c)
+			ipCipher.Decrypt(dec, enc)
+			if !bytes.Equal(dec, c) {
+				t.Errorf("expected %s have %s:", c.String(), dec.String())
+			}
+		}
+	})
+}
+
+func TestDecrypt(t *testing.T) {
+	TestEncrypt(t)
+}
