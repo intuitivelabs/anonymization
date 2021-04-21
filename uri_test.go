81023342aaf2b47e232daab8eec5859e93252034
Cristian Constantin <cristian@intuitivelabs.com>
2021-04-21 18:25:35 +0200

    adds base32 codec
    - see "The Base16, Base32, and Base64 Data Encodings" https://tools.ietf.org/html/rfc4648#section-7 for details
    - encrypted URIs have to be encoded before they are sent over the wire
    - anonymized URIs received in events have to be first decoded and then
      decrypted

--- uri_test.go
+++ uri_test.go
@@ -1,6 +1,7 @@
 package anonymization
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/hex"
 	"io"
@@ -8,6 +9,52 @@ import (
 
 	"github.com/intuitivelabs/sipsp"
 )
+
+func TestBase32Codec(t *testing.T) {
+	// init
+	df := DbgOn()
+	defer DbgRestore(df)
+	uris := [...][]byte{
+		[]byte("sip:foo:pass@bar.com"),
+		[]byte("sip:foo@bar.com"),
+		[]byte("sips:foo:pass@bar.com"),
+		[]byte("sip:1234"),
+		[]byte("sip:foo"),
+	}
+	pUris := make([]sipsp.PsipURI, len(uris))
+	for i, s := range uris {
+		if err, _ := sipsp.ParseURI(s, &pUris[i]); err != 0 {
+			t.Fatalf("could not parse SIP URI: %s", string(s))
+		}
+	}
+	// tests
+	t.Run("encode / decode w/ memory allocation", func(t *testing.T) {
+		for i, u := range pUris {
+			Dbg("test case uri: %s", string(uris[i]))
+			au := AnonymURI(u)
+			l := au.EncodedLen(uris[i])
+			Dbg("encoded len: %d", l)
+			encoded := make([]byte, l)
+			if err := au.Encode(encoded, uris[i]); err != nil {
+				t.Fatalf("cannot encode URI %s: %s", uris[i], err.Error())
+			}
+			Dbg("encoded URI: %v (len: %d)", encoded, len(encoded))
+			//Dbg("encoded URI: %s", au.Str*ing(encoded))
+			Dbg("encoded URI: %s", string((*sipsp.PsipURI)(&au).Flat(encoded)))
+			l = au.DecodedLen(encoded)
+			decoded := make([]byte, l)
+			if err := au.Decode(decoded, encoded); err != nil {
+				Dbg("decoded URI: %v", decoded)
+				t.Fatalf("cannot decode URI %s: %s", uris[i], err.Error())
+			}
+			Dbg("decoded URI: %s", string((*sipsp.PsipURI)(&au).Flat(decoded)))
+			uri := sipsp.PsipURI(au)
+			if !bytes.Equal(uris[i], uri.Flat(decoded)) {
+				t.Fatalf(`expected: "%s" got: "%s"`, uris[i], string(decoded))
+			}
+		}
+	})
+}
 
 func TestCBCEncrypt(t *testing.T) {
 	// init
@@ -23,6 +70,7 @@ func TestCBCEncrypt(t *testing.T) {
 	// test case data
 	uris := [...][]byte{
 		[]byte("sip:foo:pass@bar.com"),
+		[]byte("sip:foo@bar.com"),
 		[]byte("sips:foo:pass@bar.com"),
 		[]byte("sip:1234"),
 		[]byte("sip:foo"),
@@ -34,32 +82,63 @@ func TestCBCEncrypt(t *testing.T) {
 		}
 	}
 	// tests
-	t.Run("encrypt / decrypt ", func(t *testing.T) {
+	t.Run("encrypt / decrypt w/ memory allocation", func(t *testing.T) {
 		for i, u := range pUris {
-			Dbg("test case uri: %s\n", string(uris[i]))
+			Dbg("test case uri: %s", string(uris[i]))
 			au := AnonymURI(u)
 			l, err := au.PKCSPaddedLen(cipher.User.Encrypter.BlockSize())
 			if err != nil {
 				t.Fatalf("cannot compute URI pad len %s: %s", uris[i], err.Error())
 			}
-			Dbg("padded len: %d\n", l)
+			Dbg("padded len: %d", l)
 			ciphertxt := make([]byte, l)
 			if err := au.CBCEncrypt(ciphertxt, uris[i]); err != nil {
 				t.Fatalf("cannot encrypt URI %s: %s", uris[i], err.Error())
 			}
-			Dbg("encrypted URI: %v (len: %d)\n", ciphertxt, len(ciphertxt))
+			Dbg("encrypted URI: %v (len: %d)", ciphertxt, len(ciphertxt))
 			plaintxt := make([]byte, len(ciphertxt))
 			if err := au.CBCDecrypt(plaintxt, ciphertxt); err != nil {
-				Dbg("decrypted URI: %v\n", plaintxt)
+				Dbg("decrypted URI: %v", plaintxt)
 				t.Fatalf("cannot decrypt URI %s: %s", uris[i], err.Error())
 			}
 			if au.User.Len > 0 {
-				Dbg("decrypted URI: %v %s%s@%s\n", plaintxt,
+				Dbg("decrypted URI: %v %s%s@%s", plaintxt,
 					string(plaintxt[au.Scheme.Offs:au.Scheme.Offs+au.Scheme.Len]),
 					string(plaintxt[au.User.Offs:au.User.Offs+au.User.Len]),
 					string(plaintxt[au.Host.Offs:au.Host.Offs+au.Host.Len]))
 			} else {
-				Dbg("decrypted URI: %v %s%s\n", plaintxt,
+				Dbg("decrypted URI: %v %s%s", plaintxt,
+					string(plaintxt[au.Scheme.Offs:au.Scheme.Offs+au.Scheme.Len]),
+					string(plaintxt[au.Host.Offs:au.Host.Offs+au.Host.Len]))
+			}
+		}
+	})
+	t.Run("encrypt / decrypt w/ static memory", func(t *testing.T) {
+		for i, u := range pUris {
+			Dbg("test case uri: %s", string(uris[i]))
+			au := AnonymURI(u)
+			l, err := au.PKCSPaddedLen(cipher.User.Encrypter.BlockSize())
+			if err != nil {
+				t.Fatalf("cannot compute URI pad len %s: %s", uris[i], err.Error())
+			}
+			Dbg("padded len: %d", l)
+			ciphertxt := EncryptBuf()
+			if err := au.CBCEncrypt(ciphertxt, uris[i]); err != nil {
+				t.Fatalf("cannot encrypt URI %s: %s", uris[i], err.Error())
+			}
+			Dbg("encrypted URI: %v (len: %d)", ciphertxt, len(ciphertxt))
+			plaintxt := DecryptBuf()
+			if err := au.CBCDecrypt(plaintxt, ciphertxt); err != nil {
+				Dbg("decrypted URI: %v", plaintxt)
+				t.Fatalf("cannot decrypt URI %s: %s", uris[i], err.Error())
+			}
+			if au.User.Len > 0 {
+				Dbg("decrypted URI: %v %s%s@%s", plaintxt,
+					string(plaintxt[au.Scheme.Offs:au.Scheme.Offs+au.Scheme.Len]),
+					string(plaintxt[au.User.Offs:au.User.Offs+au.User.Len]),
+					string(plaintxt[au.Host.Offs:au.Host.Offs+au.Host.Len]))
+			} else {
+				Dbg("decrypted URI: %v %s%s", plaintxt,
 					string(plaintxt[au.Scheme.Offs:au.Scheme.Offs+au.Scheme.Len]),
 					string(plaintxt[au.Host.Offs:au.Host.Offs+au.Host.Len]))
 			}
