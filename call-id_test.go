953d2d29f0a17c0944a4d69c17d6f8a373438cb4
Cristian Constantin <cristian@intuitivelabs.com>
2021-06-28 11:15:21 +0200

    adds unit test for call-id anonymization

--- call-id_test.go
+++ call-id_test.go
@@ -127,3 +127,62 @@ func TestCallIdCBCEncrypt(t *testing.T) {
 	})
 	// clean-up
 }
+
+func TestCallIdAnonymization(t *testing.T) {
+	// init
+	df := DbgOn()
+	defer DbgRestore(df)
+	var encKey [EncryptionKeyLen]byte
+	pass := "foobar"
+	GenerateKeyFromPassphraseAndCopy(pass, EncryptionKeyLen, encKey[:])
+
+	// initialize the URI CBC based encryption
+	InitCallIdKeysFromMasterKey(encKey[:], EncryptionKeyLen)
+	NewCallIdCBC(GetCallIdKeys())
+	// test case data
+	callIds := [...][]byte{
+		[]byte("AB170EB876CF1542@188.74.1.5"),
+		[]byte("34358C152D2E461B@134.19.92.222"),
+		[]byte("E996A63EBD130CF4@134.19.43.69"),
+		[]byte("2311242011@192_168_42_93"),
+		[]byte("CC7893C861067F60@188.74.19.112"),
+		[]byte("lbSVa-MrbY9KqwGJfVQJfQ.."),
+		[]byte("2582469A238E481B@188.74.0.26"),
+		[]byte("977BAAC8F30E4B10@77.182.102.199"),
+		[]byte("66063557F19E4A73@134.19.44.57"),
+		[]byte("b850d06d-2d87-1224-afa1-00095200ed73"),
+		[]byte("3154A63A30C3CEF7@178.201.226.73"),
+		[]byte("6E43D0571A0323EC@178.201.226.73"),
+	}
+	pCallIds := make([]sipsp.PCallIDBody, len(callIds))
+	for i, s := range callIds {
+		pCallIds[i].Reset()
+		if _, err := sipsp.ParseCallIDVal([]byte(string(s)+"\r\n"), 0, &pCallIds[i]); (err != sipsp.ErrHdrOk) && (err != sipsp.ErrHdrMoreBytes) {
+			t.Fatalf(`error parsing SIP Call-ID "%s": %v`, string(s), err)
+		}
+	}
+	// tests
+	t.Run("dynamic memory", func(t *testing.T) {
+		for i, c := range pCallIds {
+			Dbg("test case Call-ID: %s", string(callIds[i]))
+			ac := AnonymPField{
+				PField: c.CallID,
+			}
+			anonym := make([]byte, 4*len(callIds[i]))
+			if err := ac.Anonymize(anonym, callIds[i]); err != nil {
+				t.Fatalf("cannot anonymize Call-ID %s: %s", callIds[i], err.Error())
+			}
+			Dbg("anonymized Call-ID: %v %s (len: %d)", ac.PField.Get(anonym), string(ac.PField.Get(anonym)), len(ac.PField.Get(anonym)))
+			plaintxt := make([]byte, 2*len(callIds[i]))
+			if err := ac.Deanonymize(plaintxt, anonym); err != nil {
+				Dbg("decrypted Call-ID: %v", plaintxt)
+				t.Fatalf("cannot decrypt Call-ID %s: %s", callIds[i], err.Error())
+			}
+			Dbg("deanonymized Call-ID: %v %s", plaintxt, string(ac.PField.Get(plaintxt)))
+			if !bytes.Equal(callIds[i], ac.PField.Get(plaintxt)) {
+				t.Fatalf(`expected: "%s" got: "%s"`, callIds[i], string(ac.PField.Get(plaintxt)))
+			}
+		}
+	})
+	// clean-up
+}
