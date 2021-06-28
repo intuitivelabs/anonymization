3f2fb8a305295258bad5327de2d8574644c54aaa
Cristian Constantin <cristian@intuitivelabs.com>
2021-06-28 11:14:15 +0200

    adds debug msg

--- call-id.go
+++ call-id.go
@@ -42,10 +42,14 @@ func InitCallIdKeys(iv []byte, k []byte) {
 }
 
 func InitCallIdKeysFromMasterKey(masterKey []byte, keyLen int) {
+	df := DbgOn()
+	defer DbgRestore(df)
 	// generate Call-ID IV for CBC
 	GenerateCallIdIV(masterKey[:], EncryptionKeyLen, GetCallIdKeys().IV[:])
+	Dbg("Call-ID IV: %v", GetCallIdKeys().IV)
 	// generate key for Call-ID
 	GenerateCallIdKey(masterKey[:], EncryptionKeyLen, GetCallIdKeys().Key[:])
+	Dbg("Call-ID Key: %v", GetCallIdKeys().Key)
 }
 
 func NewCallIdCBC(keys *CallIdKeys) *BlockModeCipher {
