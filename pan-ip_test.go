4f8c08891ff94e8bcd5d838eb63d9827b42646a7
Cristian Constantin <cristian@intuitivelabs.com>
2021-11-12 18:53:18 +0100

    uses a local PanIPv4 var for each test

--- pan-ip_test.go
+++ pan-ip_test.go
@@ -6,12 +6,15 @@ import (
 	"testing"
 )
 
-func TestPanIP(t *testing.T) {
+func TestPanIPv4(t *testing.T) {
 	// set-up if needed
 	key := [16]byte{21, 34, 23, 141, 51, 164, 207, 128, 19, 10, 91, 22, 73, 144, 125, 16}
 	iv := [16]byte{216, 152, 143, 131, 121, 121, 101, 39, 98, 87, 76, 45, 42, 132, 34, 2}
-	pan := GetPan4().WithKeyAndIV(key, iv).WithBitsPrefixBoundary(EightBitsPrefix)
-	t.Run("IPv4", func(t *testing.T) {
+	var encKey [EncryptionKeyLen]byte
+	pass := "foobar"
+	GenerateKeyFromPassphraseAndCopy(pass, EncryptionKeyLen, encKey[:])
+	t.Run("random keys", func(t *testing.T) {
+		var enc, dec net.IP
 		cases := []net.IP{
 			[]byte{24, 5, 0, 80},
 			[]byte{22, 11, 33, 44},
@@ -21,7 +24,31 @@ func TestPanIP(t *testing.T) {
 			[]byte{1, 2, 8, 9},
 			[]byte{1, 2, 3, 10},
 		}
+		pan := GetPan4().WithKeyAndIV(key, iv).WithBitsPrefixBoundary(EightBitsPrefix)
+		enc = make([]byte, net.IPv4len)
+		dec = make([]byte, net.IPv4len)
+		for _, c := range cases {
+			pan.Encrypt(enc, c)
+			t.Logf("plain: %s encrypted: %s", c.String(), enc.String())
+			pan.Decrypt(dec, enc)
+			t.Logf("encrypted: %s decrypted: %s", enc.String(), dec.String())
+			if !bytes.Equal(dec, c) {
+				t.Errorf("expected: %s have: %s (enc: %s)", c.String(), dec.String(), enc.String())
+			}
+		}
+	})
+	t.Run("password keys", func(t *testing.T) {
 		var enc, dec net.IP
+		cases := []net.IP{
+			[]byte{24, 5, 0, 80},
+			[]byte{22, 11, 33, 44},
+			[]byte{255, 0, 255, 241},
+			[]byte{1, 2, 3, 4},
+			[]byte{1, 5, 6, 7},
+			[]byte{1, 2, 8, 9},
+			[]byte{1, 2, 3, 10},
+		}
+		pan := GetPan4().WithMasterKey(encKey[:]).WithBitsPrefixBoundary(EightBitsPrefix)
 		enc = make([]byte, net.IPv4len)
 		dec = make([]byte, net.IPv4len)
 		for _, c := range cases {
