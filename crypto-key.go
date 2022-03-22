8513f55495d24e06d7cee332a54e6f66bbeebf8a
Cristian Constantin <cristian@intuitivelabs.com>
2022-03-22 12:46:09 +0100

    adds documentation to crypto-key.go file

--- crypto-key.go
+++ crypto-key.go
@@ -23,7 +23,7 @@ import (
 //   1.1. generate the master key from passphrase using `PBKDF2` key derivation function with HMAC-SHA1 and `IpcipherSalt` salt;
 // 2. Input: a binary master key
 //   2.1. (optional) if the key is encoded using hex format decoded it first to get the key in binary format
-//   2.2. generate the authentication key from the master key using `PBKDF2` key derivation function with HMAC-SHA1 and 'IpcipherrSalt';
+//   2.2. generate the authentication key from the master key using `PBKDF2` key derivation function with HMAC-SHA1 and 'IpcipherSalt';
 //   2.3. generate the PAN encryption key and IV from the master key using `PBKDF2` key derivation function with HMAC-SHA1 and `PanSalt` salt;
 //   2.4. generate the URI Username encryption key and IV from the master key using `PBKDF2` key derivation function with HMAC-SHA1 and `UriUsernameSalt` salt;
 //   2.5. generate the URI Host encryption key and IV from the master key using `PBKDF2` key derivation function with HMAC-SHA1 and `UriHostSalt` salt;
@@ -70,7 +70,7 @@ const (
 	LastKey, _ // marker, not used
 )
 
-// keying material used for crypto algorithms: authentican key, encryption key, IV
+// keying material used for crypto algorithms: authentication key, encryption key, IV
 type KeyingMaterial struct {
 	Master [EncryptionKeyLen]byte
 	Auth   [AuthenticationKeyLen]byte
@@ -86,32 +86,38 @@ func GetKeys() []KeyingMaterial {
 }
 
 // NewKeyingMaterial generates new keys using `masterKey`, `salt` and PBKDF2 with HMAC-SHA1
+// masterKey is copied in the returned KeyingMaterial.
 func NewKeyingMaterial(masterKey []byte, salt *Salt) *KeyingMaterial {
 	km := KeyingMaterial{}
 	df := DbgOn()
 	defer DbgRestore(df)
-	km.generate(masterKey, salt)
+	km.SetMasterKey(masterKey)
+	km.Generate(salt)
 	return &km
 }
 
-// generate generates the keying material (authentication, encryption key and initialization vector) based on masterKey and salt.
-// masterKey is also copied in the returned KeyingMaterial.
-func (km *KeyingMaterial) generate(masterKey []byte, salt *Salt) *KeyingMaterial {
+// SetMasterKey copies the key into km.Master. km.Master is going to be used as a master key from which all the other keys are derived.
+func (km *KeyingMaterial) SetMasterKey(key []byte) {
+	subtle.ConstantTimeCopy(1, km.Master[:], key[:])
+}
+
+// generate generates the keying material (authentication, encryption key and initialization vector) based
+// on salt and the internally stored master key.
+func (km *KeyingMaterial) Generate(salt *Salt) *KeyingMaterial {
 	df := DbgOn()
 	defer DbgRestore(df)
-	subtle.ConstantTimeCopy(1, km.Master[:], masterKey[:])
 	// generate IV
-	if err := GenerateKeyWithSaltAndCopy(salt.IV, masterKey[:], EncryptionKeyLen, km.IV[:]); err != nil {
+	if err := GenerateKeyWithSaltAndCopy(salt.IV, km.Master[:], EncryptionKeyLen, km.IV[:]); err != nil {
 		panic(err)
 	}
 	_ = WithDebug && Dbg("IV: %v", km.IV[:])
 	// generate encryption key
-	if err := GenerateKeyWithSaltAndCopy(salt.Key, masterKey[:], EncryptionKeyLen, km.Enc[:]); err != nil {
+	if err := GenerateKeyWithSaltAndCopy(salt.Key, km.Master[:], EncryptionKeyLen, km.Enc[:]); err != nil {
 		panic(err)
 	}
 	_ = WithDebug && Dbg("encryption key: %v", km.Enc[:])
 	// generate authentication key
-	GenerateKeyFromBytesAndCopy(masterKey[:], AuthenticationKeyLen, km.Auth[:])
+	GenerateKeyFromBytesAndCopy(km.Master[:], AuthenticationKeyLen, km.Auth[:])
 	_ = WithDebug && Dbg("authentication key: %v", km.Auth[:])
 	return km
 }
@@ -148,6 +154,11 @@ func GenerateAllKeysWithPassphrase(passphrase string) {
 	}
 }
 
+// GenerateKeyWithSalt derives a key using:
+// - salt which is a hex encoded string
+// - bytes which is the input "password"
+// The key is derived using PBKDF2 with HMAC-SHA1, iteration count being the constant 'IterationCount'
+// and returned as a byte slice.
 func GenerateKeyWithSalt(salt string, bytes []byte, keyLen int) ([]byte, error) {
 	decoded, err := hex.DecodeString(salt)
 	if err != nil {
@@ -156,6 +167,11 @@ func GenerateKeyWithSalt(salt string, bytes []byte, keyLen int) ([]byte, error) 
 	return pbkdf2.Key(bytes, decoded, IterationCount, keyLen, sha1.New), nil
 }
 
+// GenerateKeyWithSaltAndCopy derives a key using:
+// - salt which is a hex encoded string
+// - bytes which is the input "password"
+// The key is derived using PBKDF2 with HMAC-SHA1, iteration count being the constant 'IterationCount'
+// and copied in the provided byte slice.
 func GenerateKeyWithSaltAndCopy(salt string, bytes []byte, keyLen int, key []byte) error {
 	tmpKey, err := GenerateKeyWithSalt(salt, bytes, keyLen)
 	if err != nil {
@@ -165,20 +181,40 @@ func GenerateKeyWithSaltAndCopy(salt string, bytes []byte, keyLen int, key []byt
 	return nil
 }
 
+// GenerateKeyFromBytes derives a key using:
+// - binary constant `SaltIpcipher` salt
+// - bytes which is the input "password"
+// The key is derived using PBKDF2 with HMAC-SHA1, iteration count being the constant 'IterationCount'
+// and returned as a byte slice.
 func GenerateKeyFromBytes(bytes []byte, keyLen int) []byte {
 	return pbkdf2.Key(bytes, []byte(SaltIpcipher), IterationCount, keyLen, sha1.New)
 }
 
+// GenerateKeyFromBytesAndCopy derives a key using:
+// - binary constant `SaltIpcipher` salt
+// - bytes which is the input "password"
+// The key is derived using PBKDF2 with HMAC-SHA1, iteration count being the constant 'IterationCount'
+// and copied in the provided byte slice.
 func GenerateKeyFromBytesAndCopy(bytes []byte, keyLen int, key []byte) {
 	tmpKey := GenerateKeyFromBytes(bytes, keyLen)
 	subtle.ConstantTimeCopy(1, key[:], tmpKey[:])
 	return
 }
 
+// GenerateKeyFromPassphrase derives a key using:
+// - binary constant `SaltIpcipher` salt
+// - passphrase which is the input "password"
+// The key is derived using PBKDF2 with HMAC-SHA1, iteration count being the constant 'IterationCount'
+// and returned as a byte slice.
 func GenerateKeyFromPassphrase(passphrase string, keyLen int) []byte {
 	return GenerateKeyFromBytes([]byte(passphrase), keyLen)
 }
 
+// GenerateKeyFromPassphraseAndCopy derives a key using:
+// - binary constant `SaltIpcipher` salt
+// - bytes which is the input "password"
+// The key is derived using PBKDF2 with HMAC-SHA1, iteration count being the constant 'IterationCount'
+// and copied in the provided byte slice.
 func GenerateKeyFromPassphraseAndCopy(passphrase string, keyLen int, key []byte) {
 	GenerateKeyFromBytesAndCopy([]byte(passphrase), keyLen, key)
 	return
